8e5420f430d5827378c58ef4ac0febccafaa3ec0
Bishal Bhattarai <bishalbhattarai921@gmail.com>
2023-12-09 09:02:18 +0545

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,43 @@
-import React from "react";
-import './index.css'
-import { useState } from "react";
-import { useEffect } from "react";
-import UserData from "./components/UserData";
+import {useEffect, useState} from "react";
+import UserData from "./components/UserData.jsx";
+
 const API = "https://jsonplaceholder.typicode.com/users";
 
-function App() {
-  const [users, setUsers] = useState([""]);
-  const fetchUsers = async (url) => {
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      if (data.length > 0) {
-        setUsers(data);
-      }
-      console.log(data);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-  useEffect(() => {
-    fetchUsers(API);
-  }, []);
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
+const App = () => {
+    const [users, setUsers] = useState([]);
 
-          <th>ID</th>
-          <th>NAME</th>
-          <th>EMAIL</th>
-          <th>ADDRESS</th>
-          </tr>
-        </thead>
-        <tbody>
-          <UserData  users={users} />
-        </tbody>
-      </table>
-    </div>
-  );
+    const fetchUsers = async (url) => {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            if (data.length > 0) {
+                setUsers(data);
+            }
+            console.log(data);
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+
+    useEffect(() => {
+        fetchUsers(API);
+    }, [])
+    return <>
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+            </tr>
+            </thead>
+            <tbody>
+            <UserData users={users}/>
+            </tbody>
+        </table>
+    </>
 }
 
 export default App;
